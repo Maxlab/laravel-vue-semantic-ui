688f77c940c3332f0d6c324a330d56b378417a0b
Nikolay Stepanov <nstepanovdev@gmail.com>
2017-04-14 18:03:36 +0400

    add semantic.json

--- src/VueSemanticUiServiceProvider.php
+++ src/VueSemanticUiServiceProvider.php
@@ -15,6 +15,7 @@ class VueSemanticUiServiceProvider extends ServiceProvider
             __DIR__.'/../src/resources/assets' => resource_path('/assets'),
             __DIR__.'/../src/resources/views' => resource_path('/views'),
             __DIR__.'/../package.json' => base_path('/package.json'),
+            __DIR__.'/../semantic.json' => base_path('/semantic.json'),
             __DIR__.'/../gulpfile.js' => base_path('/gulpfile.js'),
             __DIR__.'/../webpack.mix.js' => base_path('/webpack.mix.js'),
         ], 'laravel-vue-semantic-ui');
